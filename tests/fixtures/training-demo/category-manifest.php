fdd59e0e41226219bec81a4ed14f396e4f53f3f4
Nicolás Ávila <nicolasavilapizarro@gmail.com>
2026-08-12 11:31:12 -0400

    test(training): add reproducible demo dataset

---
+++ tests/fixtures/training-demo/category-manifest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'vecised' => ['taxonomy'=>'product_cat','slug'=>'vecised','name'=>'VeciSed'],
+    'vecifrio' => ['taxonomy'=>'product_cat','slug'=>'friovecino','name'=>'Frío Vecino'],
+    'despensa' => ['taxonomy'=>'product_cat','slug'=>'vecidespensa','name'=>'VeciDespensa'],
+    'antojo' => ['taxonomy'=>'product_cat','slug'=>'veciantojo','name'=>'VeciAntojo'],
+    'limpieza' => ['taxonomy'=>'product_cat','slug'=>'veciaseo','name'=>'VeciAseo'],
+    'picoteo' => ['taxonomy'=>'product_cat','slug'=>'picoteoveci','name'=>'Picoteo Veci'],
+    'horno' => ['taxonomy'=>'product_cat','slug'=>'panaderia','name'=>'Panadería'],
+    'feria' => ['taxonomy'=>'product_cat','slug'=>'veciferia','name'=>'Veciferia'],
+];
